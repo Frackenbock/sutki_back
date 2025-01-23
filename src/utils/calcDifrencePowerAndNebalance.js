088dea43c1a28449acbeea3ac93e561426f6505e
Frackenbock <rasaw1992@gmail.com>
2025-01-23 13:09:14 +0300

    modified:   src/utils/calcDifrencePowerAndNebalance.js

--- src/utils/calcDifrencePowerAndNebalance.js
+++ src/utils/calcDifrencePowerAndNebalance.js
@@ -15,7 +15,11 @@ function calcDifrencePowerAndNebalance (arrAIIS,arrPBR){
         if(arrAIIS[i]===0){
             nebalance.push(0);
         }else{
-            nebalance.push(((Number(arrAIIS[i])-Number(arrPBR[i]))/Number(arrPBR[i])*100).toFixed(3))
+            if(Number(arrPBR[i])===0){
+                nebalance.push(0)
+            }else{
+                nebalance.push(((Number(arrAIIS[i])-Number(arrPBR[i]))/Number(arrPBR[i])*100).toFixed(3))
+            }
         }
     }
     return {differenceAiisAndPbr,nebalance}
